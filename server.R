9456b7d83d2f96639cd46fcea9c5b7098800813d
Connor Chan <cachan25@uw.edu>
2023-03-08 00:52:12 -0800

    Intro/conclusion Update

--- server.R
+++ server.R
@@ -24,8 +24,18 @@ server <- function(input, output) {
      
      #intro
      output$intro_desc <- renderText({
-          "This is the intro page."
-     })
+          "For our final project deliverable, we examined variables including, but not limited to, race, gender, and nominees/winners within the Oscars' history. To ensure a wide pool of data and representation in the film industry, we chose an Oscars dataset spanning between 1927 and 2020. This aspect is important because we hope to provide findings to push toward additional diversity and inclusivity within the film industry. In addition to the three variables in focus, we were also introduced to variables including release date, nomination year, categories, and ceremony number. Although we focused on specific data within the dataset, we used all provided information to best inform our audience with our findings.
+To best showcase our knowledge, we created three visualizations in the form of a bar graph, pie chart, and line graph. These graphs represent the “Top N Films to Display,” “Oscar Wins by Race,” and “Oscar Winners by Gender Per Year;” the graphs showcase our data in a visual manner, while also providing an interactive aspect for the audience’s understanding of the material.
+To create these visualizations, we compiled variable data related questions including: 1. Which nominated films have the most amount of wins? 2. How is the spectrum of racial groups represented in the Oscars, specifically the Oscar winners? 3. Is there equal representation of men and women within the category of Oscar winners? These are the main things we are going to be examining, but we also look to make other discoveries along the way.
+
+The dataset is sourced from Kaggle, which is a hub of online datasets. This specific dataset was created by Dharmik Donga. In the description, Donga mentions, “I found a basic dataset from Kaggle, but it did not have gender and race of the nominations and winners.” As a result, Donga most likely drew inspiration from another dataset and added gender and race categories. Donga also gives an acknowledgment to Raphael Fontes, who is the source for the basic dataset.
+The main thing we need to consider is the limited data availability: The dataset only covers the nominations and winners from 1927 to 2020 and could be considered not as complete as there is no information available past 2020. Additionally, there could be biases in the Academy's voting process: The Academy's voting decides the nominations and winners, and might not be representative of the wider population. Another thing to take into account is user error, as Donga may have incorrectly transferred completely accurate data before publication of his dataset on Kaggle.
+There could also be some discrepancies within how the categories are recorded. Within the dataset, the only things recorded as variables are for cinematography, writing, best picture, best actress/supporting actress, and best actor/supporting actor. As a result, the data is missing all of the other very important categories that come into play when making a movie. Another aspect to consider is how measuring/voting on these categories/nominees has changed over time, as the process in 1927 is possibly different than the process in 2020, and probably even again in 2023. These aspects, which are not in our control, potentially adjust dataset's criteria.
+
+
+URL link to the original source of the data:
+[https://www.kaggle.com/datasets/dharmikdonga/academy-awards-dataset-oscars?resource=download]"
+          })
      
      output$oscars1_plot <- renderPlotly({
           
@@ -115,7 +125,11 @@ server <- function(input, output) {
      })
      #conclusion
      output$conclusion_desc <- renderText({
-          "This is the conclusion page."
+          "In conclusion, our project analyzed various aspects of the Oscars including categories such as race, gender, nominees, and winners to contribute to the larger discussion on diversity and inclusivity within the film industry. Our findings provide important insights into the representation of marginalized groups in the cinamtic industry.
+One major takeaway from our analysis is that it is extremely uncommon for films to surpass the threshold of 9 wins, with only four films ever achieving this feat in the history of the Oscars. This highlights the difficulty of creating a film that is not only critically acclaimed, but also successful in winning multiple awards.
+Another major takeaway is that people who are white encompass 95.2% of all Oscar winners. This statistic shows the lack of representation of people of color in the film industry, despite the increasing awareness and efforts towards greater diversity.
+Lastly, our analysis revealed that in the history of the Oscars, there have always been more men than women winners. However, in the past century, men winners have steadily decreased, while women winners continue to increase in Oscar winnings. This trend indicates a shift towards greater representation and inclusivity for women in the film industry.
+Despite the limitations of our dataset, our project provides a starting point for future research and discussion on the topic of diversity and inclusivity within the film industry. Further analysis of marginalized groups, such as individuals with disabilities or non-binary gender identities, or in independent films, could contribute to a more comprehensive understanding of the challenges these marginalized groups face in the industry. By continuing to engage in dialogue, we can work towards creating a more representative industry for all."
      })
      
 }
